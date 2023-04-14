347a445eac3ad667a74a150fa5020f21bd101bbe
poxstone <poxstone@gmail.com>
2023-04-13 23:22:27 -0500

    Update OperadorController.java

--- src/main/java/com/brisasparking/controller/OperadorController.java
+++ src/main/java/com/brisasparking/controller/OperadorController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.brisasparking.model.OperadorRepository;
-import com.brisasparking.model.ClienteModel;
 import com.brisasparking.model.OperadorModel;
 
 @RestController
